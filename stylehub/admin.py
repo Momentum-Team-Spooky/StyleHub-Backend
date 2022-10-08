fc9eccfd513efea294eefebd4aac513c4019f525
cbmosley <cbmosley@ncsu.edu>
2022-10-08 16:01:47 -0400

    Created Custom User Model and made migrations.

--- stylehub/admin.py
+++ stylehub/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin 
 
+from .models import CustomUser 
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ["email", "username",]
+
+admin.site.register(CustomUser, CustomUserAdmin)
